51bd01eb6a5ae86554bc8934d5a5abe481a77061
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-02-11 08:45:33 -0600

    Add print_report function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def count_words(text):
     word_list = text.split()
     return len(word_list)
 
+
 def count_letters(text):
     count_dict = {}
 
@@ -15,12 +16,42 @@ def count_letters(text):
     return count_dict
 
 
+def convert_dict(count_dict):
+    count_list = []
+    for letter in count_dict:
+        count_list.append({"letter": letter, "count": count_dict[letter]})
+    return count_list
+
+
+def sort_key(dict):
+    return dict["count"]
+
+
+def print_report(text, path):
+    header = f"--- Begin report for {path} ---"
+    footer = "--- End report ---"
+    word_count_report = f"{count_words(text)} words found in the document \n\n"
+
+    count_list = convert_dict(count_letters(text))
+    count_list.sort(reverse=True, key=sort_key)
+
+    print(header)
+    print(word_count_report)
+
+    for item in count_list:
+        letter = item["letter"]
+        count = item["count"]
+        print(f"The '{letter}' character was found {count} times")
+
+    print(footer)
+
 
 def main():
-    with open('./assets/frankenstein.txt', 'r') as file:
+    path = './assets/frankenstein.txt'
+    with open(path, 'r') as file:
         contents = file.read()
 
-        print(count_letters(contents))
+        print_report(contents, path)
 
 
 main()
